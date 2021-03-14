a495e54c85d9281f4248aff243fc65324449cb6b
Behal <behalserhii@gmail.com>
2021-03-14 20:22:28 +0200

    [add] TKs to test_product_page.py - test_guest_should_see_login_link_on_product_page - test_guest_can_go_to_login_page_from_product_page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 
@@ -63,3 +64,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.click_add_to_basket()
     product_name = product_page.get_product_name()
     product_page.message_is_disappeared(f"{product_name} has been added to your basket.")
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser=browser, url=browser.current_url)
+    login_page.should_be_login_page()
